6467ccdd6a55d4c9b463782d4e2110fddd6428ae
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-12-01 18:05:53 +0500

    second task

--- day01/task.py
+++ day01/task.py
@@ -2,5 +2,13 @@
 with open('input.txt', 'r') as f:
     input = list(map(int, f.read().split('\n')))
 
-res = sum([1 if x < y else 0 for x, y in zip(input, input[1:])])
-print(res)
+def first():
+    res = sum([1 if x < y else 0 for x, y in zip(input, input[1:])])
+    print(res)
+
+def second():
+    windows = [x+y+z for x,y,z in zip(input, input[1:], input[2:])]
+    res = sum([1 if x < y else 0 for x, y in zip(windows, windows[1:])])
+    print(res)
+
+second()
