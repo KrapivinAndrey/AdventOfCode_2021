752fcb660d3bf7883d87b7a726be920eeaf9e58d
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-12-02 23:01:22 +0500

    Второе задание

--- day02/task.py
+++ day02/task.py
@@ -12,4 +12,22 @@ def first():
     print(f"ans {pos*depth}")
 
 
+def second():
+    def move(prev, el):
+        if el[0] == "forward":
+            return [prev[0]+int(el[1]), prev[1]+int(el[1])*prev[2], prev[2]]
+        elif el[0] == "up":
+            return [prev[0], prev[1], prev[2] - int(el[1])]
+        elif el[0] == "down":
+            return [prev[0], prev[1], prev[2] + int(el[1])]
+
+    from functools import reduce
+    res = reduce(move, input, [0, 0, 0])
+    print(f"pos {res[0]}")
+    print(f"depth {res[1]}")
+
+    print(f"ans {res[0] * res[1]}")
+
+
 first()
+second()
