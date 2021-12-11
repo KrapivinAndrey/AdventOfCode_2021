31fbec5d88cfdd7c8c1fb5c4002f6c63e8ce4aee
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-12-11 13:20:19 +0500

    Классика. Разбор скобок

--- day10/task.py
+++ day10/task.py
@@ -1,8 +1,9 @@
+
 with open("input.txt") as f:
     input = list(map(lambda x: x.strip(), f.readlines()))
 
 
-def check_error(brackets, return_queue = False):
+def check_error(brackets):
     queue = []
     opens = {"(", "[", "{", "<"}
     for char in brackets:
@@ -18,15 +19,45 @@ def check_error(brackets, return_queue = False):
                 or last == "<" and char == ">"
             ):
                 return char
-    if return_queue:
-        return queue
+
+def check_incomplete(brackets):
+    queue = []
+    opens = {"(", "[", "{", "<"}
+    for char in brackets:
+        if char in opens:
+            queue.append(char)
+        else:
+            last = queue.pop()
+
+            if not (
+                last == "(" and char == ")"
+                or last == "[" and char == "]"
+                or last == "{" and char == "}"
+                or last == "<" and char == ">"
+            ):
+                return []
+
+    return queue
 
 
 def first():
-    coast = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
-    result = sum([coast[check_error(x)] for x in input])
+    cost = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
+    result = sum([cost[check_error(x)] for x in input])
     print(result)
 
 
+def second():
+    def points(ending):
+        cost = {'(': 1, '[': 2, '{': 3, '<': 4}
+        result = 0
+        while ending:
+            result = result * 5 + cost[ending.pop()]
+
+        return result
+
+    points = list(filter((0).__ne__, [points(check_incomplete(x)) for x in input]))
+    points.sort()
+    print(points[len(points) // 2])
 
 
+second()
